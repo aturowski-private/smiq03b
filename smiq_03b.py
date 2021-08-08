ae84ccff99f2b5a22ed6a9e641933fa751d96d2f
Adam Turowski <a.turowski@ymail.com>
2021-08-08 16:52:54 +0100

    WIP

--- smiq_03b.py
+++ smiq_03b.py
@@ -1,6 +1,7 @@
 import sys
 import serial
 import io
+import time
 
 ARB_MEM_ADDR = 0  # the start in ARB memory where the waveform is going to be written
 WAVEFORM_NAME = 'TEST_1'
@@ -18,6 +19,7 @@ class SMIQ3B:
     self.port = port
     self.eol = eol
     self.write(self.eol)  # switch SMIQ03B to remote RS232 mode
+    time.sleep(0.1)       # wait for SMIQ03B to switch over before issuing other commands
     self.identify()
     
 
@@ -56,8 +58,38 @@ class SMIQ3B:
     # -1.0 -> 768
     return int(32000.0*s_float + 32768.0)
 
+  def write_waveform(self, name, clock_hz, samples):
+    # convert samples to integer form as expected by DAC in AWG in SMIQ03B
+    samples_dac = []
+    for i,q in samples:
+      samples_dac.append((self.to_dac(i), self.to_dac(q)))
+
+    # convert DAC samples to waveform (two bytes value, little endian format)
+    waveform = ''
+    waveform = waveform + '{:d},#'.format(ARB_MEM_ADDR)
+    for i,q in samples_dac:
+      waveform = waveform + chr(i & 0xFF) + chr((i >> 8) & 0xFF)
+      waveform = waveform + chr(q & 0xFF) + chr((q >> 8) & 0xFF)
+  
+    # write waveform data to SMIQ03B
+    cmmd = ":MMEM:DATA '{:s}',#3131{{TYPE: WV,0}}{{CLOCK: {:.4f}}}{{WAVEFORM-{:d}: {:s}}}".format(name, clock_hz, len(waveform), waveform)
+    print(cmmd)
+    self.write(cmmd)
+# ;*opc?<LF>
+    
+
+
 if __name__ == '__main__':
   Smiq03b = SMIQ3B('COM1')
+  # create I/Q samples for the waveform (first in floating point in range <-1.0, 1.0>
+  samples = [ (-1.0, -1.0),
+              (-0.5, -0.5),
+              (0.0, 0.0),
+              (0.5, 0.5),
+              (1.0, 1.0)]
+  # and write samples to SMIQ03B
+#  Smiq03b.write_waveform('TEST_1', 25000.0, samples)
+  
 
   # ser = serial.Serial(  port=PORT,
                         # baudrate=BAUDRATE,
